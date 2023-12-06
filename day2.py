7965219f05fd3d5b805a2490d1c823daa3a0f6bc
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-12-07 01:40:08 +0530

    latest

--- day2.py
+++ day2.py
@@ -18,14 +18,14 @@ while True:
     games= s[colon+1:].split(';')
     
     isValid=True
-
+    
     for contest in games:
       for single in contest.split(','):
           [value, color]=single.strip().split(' ')
           print("The value is",value,"The color is",color)
           if int(value)>passes[color]:
             isValid=False
-            break
+            break 
     print("game is ",game,"total is",tot)
     tot+= int(game) if isValid else 0
     print("tot is",tot)
